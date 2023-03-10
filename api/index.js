80950eeb187ef5ea8549d3f1ed2195462b221758
Kopparam Sai <Kopparam.Sai@marlabs.com>
2023-03-10 18:02:32 +0530

    commit

--- api/index.js
+++ api/index.js
@@ -1,8 +1,13 @@
 import express from "express"
-
-const app = express()
+import authRoutes from "./routes/auth.js"
+import userRoutes from "./routes/users.js"
+import postRoutes from "./routes/posts.js"
+const app=express()
 
 app.use(express.json())
+app.use("/api/auth",authRoutes)
+app.use("/api/users",userRoutes)
+app.use("/api/posts",postRoutes)
 
 app.listen(8000,()=>{
     console.log("connected!")
